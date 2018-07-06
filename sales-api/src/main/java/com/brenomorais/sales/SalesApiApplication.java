07c795cd14809dea42ee3750790b625dd5cc3ccf
breno morais <breno.lopes.morais@hotmail>
2018-07-06 00:49:21 -0300

    first commit infra project init

---
+++ sales-api/src/main/java/com/brenomorais/sales/SalesApiApplication.java
@@ -0,0 +1,12 @@
+package com.brenomorais.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesApiApplication.class, args);
+	}
+}
